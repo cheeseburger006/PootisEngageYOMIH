a52cd90c6565cd0c668e398abd34612dd5c8d5aa
cheeseburger006 <matteo.giusto.q@gmail.com>
2023-03-15 17:15:57 +0100

    removed useless code

--- characters/Pootis/States/PootisTaunt.gd
+++ characters/Pootis/States/PootisTaunt.gd
@@ -3,10 +3,6 @@ extends CharacterState
 func _ready():
 	pass
 
-func _frame_44():
-	host.gain_super_meter(host.MAX_SUPER_METER)
-	host.unlock_achievement("ACH_HUSTLE", true)
-
 const PROJECTILE_X = 37	
 const PROJECTILE_Y = - 65
 
