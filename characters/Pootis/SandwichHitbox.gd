fa4023a2e400935ce110249d49a25989c8bdd010
cheeseburger006 <matteo.giusto.q@gmail.com>
2023-03-20 22:12:56 +0100

    deleted useless code

--- characters/Pootis/SandwichHitbox.gd
+++ characters/Pootis/SandwichHitbox.gd
@@ -4,5 +4,6 @@ func _ready():
 	connect("hit_something", self, "on_hit_something")
 
 func on_hit_something(obj, hitbox):
-	get_parent().enter_force_dir_x = str(float(get_parent().enter_force_dir_x) * -1);
+	var mult = Utils.int_sign(get_parent().host.fighter_owner.opponent.get_pos().x - get_parent().host.fighter_owner.get_pos().x)
+	get_parent().enter_force_dir_x = str(float(get_parent().enter_force_dir_x) * -mult)
 	get_parent().apply_enter_force()
