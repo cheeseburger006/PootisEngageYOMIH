a6d6f39a4304e71269d8418d24b3b482000782e0
cheeseburger006 <matteo.giusto.q@gmail.com>
2023-03-20 22:15:37 +0100

    updated projectile directory

--- characters/Pootis/States/PootisTaunt.gd
+++ characters/Pootis/States/PootisTaunt.gd
@@ -7,4 +7,4 @@ const PROJECTILE_X = 37
 const PROJECTILE_Y = - 65
 
 func _frame_25():
-	var obj = host.spawn_object(preload("res://Pootis Mod/characters/Pootis/Projectiles/Sandwich.tscn"), PROJECTILE_X, PROJECTILE_Y, true, {"dir": fixed.normalized_vec("0", "0")})
+	var obj = host.spawn_object(preload("res://Pootis Mod/characters/Pootis/Projectiles/Sandwich/Sandwich.tscn"), PROJECTILE_X, PROJECTILE_Y, true, {"dir": fixed.normalized_vec("0", "0")})
